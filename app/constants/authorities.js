c74a60418fc8b26dc4d75393ce1effd9742aa478
SergantChe <sergei.alex.kozlov@gmail.com>
2020-05-12 12:11:51 +0300

    change AUTHORITIES_LIST_URL

--- app/constants/authorities.js
+++ app/constants/authorities.js
@@ -1,5 +1,8 @@
 export const PUBLIC_DATA_URL =
   'https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv';
 
-export const AUTHORITIES_LIST_URL =
+export const AUTHORITIES_LIST_URL_OLD =
   'https://private-kit-sm.firebaseapp.com/authorities.yaml';
+
+export const AUTHORITIES_LIST_URL =
+  'https://private-kit-sm.web.app/authorities.yaml';
